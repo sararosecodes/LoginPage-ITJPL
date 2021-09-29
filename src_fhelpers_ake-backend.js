cc879a57982c22eaf02b20c48c1092a9dd55b354
sararosecodes <80790898+sararosecodes@users.noreply.github.com>
2021-09-29 12:43:54 -0700

    Create src_fhelpers_ake-backend.js

---
+++ src_fhelpers_ake-backend.js
@@ -0,0 +1,37 @@
+// array in local storage for registered users
+let users = JSON.parse(localStorage.getItem('users')) || [];
+    
+export function configureFakeBackend() {
+    let realFetch = window.fetch;
+    window.fetch = function (url, opts) {
+        return new Promise((resolve, reject) => {
+            // wrap in timeout to simulate server api call
+            setTimeout(() => {
+
+                // authenticate
+                if (url.endsWith('/users/authenticate') && opts.method === 'POST') {
+                    // get parameters from post request
+                    let params = JSON.parse(opts.body);
+
+                    // find if any user matches login credentials
+                    let filteredUsers = users.filter(user => {
+                        return user.username === params.username && user.password === params.password;
+                    });
+
+                    if (filteredUsers.length) {
+                        // if login details are valid return user details and fake jwt token
+                        let user = filteredUsers[0];
+                        let responseJson = {
+                            id: user.id,
+                            username: user.username,
+                            firstName: user.firstName,
+                            lastName: user.lastName,
+                            token: 'fake-jwt-token'
+                        };
+                        resolve({ ok: true, text: () => Promise.resolve(JSON.stringify(responseJson)) });
+                    } else {
+                        // else return error
+                        reject('Username or password is incorrect');
+                    }
+
+                    return;
